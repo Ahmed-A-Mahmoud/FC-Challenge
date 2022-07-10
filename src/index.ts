1ffac73c139e77d415e5e6f7eac86332b9da9a86
Shalaby2 <ahmed.shalaby2@ibm.com>
2022-07-10 10:11:16 +0200

    Handled extra comments - Added verification to db connection - Specified fallback port

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,31 @@ import { dbConfig } from "@interfaces/db.interface";
 import app from "app";
 import config from "config";
 import "dotenv/config";
+import { Server } from "http";
 import mongoose from "mongoose";
-const serverPort = process.env.PORT;
+const serverPort = process.env.PORT || 5000;
 
 const { host, port, name }: dbConfig = config.get("dbConfig");
-mongoose
-  .connect(`mongodb://${host}:${port}/${name}`)
-  .then(() => {
-    app.listen(serverPort, () => console.log(`Running on port ${serverPort}`));
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+
+const initializeConnections = () => {
+  let server: Server;
+  try {
+    mongoose.connect(`mongodb://${host}:${port}/${name}`);
+
+    // 'connected' event fires on initial connection and reconnection to MongoDB
+    mongoose.connection.on("connected", () => {
+      console.log(`Connected to MongoDB on port ${port}`);
+      server = app.listen(serverPort, () => console.log(`Server listening on port ${serverPort}`));
+    });
+
+    // 'disconnected' event firs on disconnection from MongoDB
+    mongoose.connection.on("disconnected", () => {
+      console.log("Disconnected from MongoDB");
+      server.close();
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+initializeConnections();
